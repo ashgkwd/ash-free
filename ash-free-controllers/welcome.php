51d5635dc9f3697ed67ce4cee7d2a6fd9529aff9
Ashish Gaikwad <ash.gkwd@gmail.com>
2013-10-30 20:39:11 +0530

    Added examples for Controller and View :D

---
+++ ash-free-controllers/welcome.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Author: Ashish A Gaikwad <ash.gkwd@gmail.com>
+* App: Ash Free
+* File: /welcome.php
+* Purpose: To demonstrate basic controller for Ash Free.
+*/
+
+# create class for this controller. 
+# This class must be derivative of \AshFree\Base\AshController
+class Welcome extends \AshFree\Base\AshController
+{
+	# index() is default function of any controller.
+	public function index()
+	{
+		# lets load our view.
+		$this->view->html('welcome_view');
+	}
+}
