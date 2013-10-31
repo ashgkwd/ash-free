4e6c901231fb2d538f06c863dcaddd1ebe1cb74e
Ashish Gaikwad <ash.gkwd@gmail.com>
2013-10-31 21:24:07 +0530

    Spells :p

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 /*
 * Author: Ashish A Gaikwad <ash.gkwd@gmail.com>
 * App: Ash Free
+* Copyright: 2013 All Rights Reserved
 * File: /index.php
 * Purpose: To handle all app requests. The landing page.
 */
@@ -13,7 +14,7 @@ require_once 'ash-free-base/loader.php';
 # welcome is default controller
 try
 {
-	(new Loader('dsf'))->run();
+	(new Loader('welcome'))->run();
 }
 catch (\Exception $e)
 {
